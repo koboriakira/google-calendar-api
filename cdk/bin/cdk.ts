5d3ce91c1feeb7bdb9f730ede09e0cbedb7c101a
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-01-12 03:24:03 +0900

    Refactor GoogleCalendarApi stack and tests

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,44 +2,8 @@
 /** @format */
 
 import "source-map-support/register";
-import { App } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
 import { GoogleCalendarApi } from "../lib/google_calendar_api";
-import { Template } from "aws-cdk-lib/assertions";
 
-describe("GoogleCalendarApi Stack", () => {
-  const app = new App();
-  const stack = new GoogleCalendarApi(app, "TestStack");
-
-  it("creates an IAM role", () => {
-    const assert = Template.fromStack(stack);
-    assert.resourceCountIs("AWS::IAM::Role", 1);
-    assert.hasResourceProperties("AWS::IAM::Role", {
-      AssumeRolePolicyDocument: {
-        Statement: [
-          {
-            Action: "sts:AssumeRole",
-            Effect: "Allow",
-            Principal: {
-              Service: "lambda.amazonaws.com",
-            },
-          },
-        ],
-        Version: "2012-10-17",
-      },
-    });
-  });
-
-  it("creates a Lambda layer", () => {
-    const assert = Template.fromStack(stack);
-    assert.resourceCountIs("AWS::Lambda::LayerVersion", 1);
-  });
-
-  it("creates a Lambda function", () => {
-    const assert = Template.fromStack(stack);
-    assert.resourceCountIs("AWS::Lambda::Function", 1);
-    assert.hasResourceProperties("AWS::Lambda::Function", {
-      Handler: "main.handler",
-      Runtime: "python3.11",
-    });
-  });
-});
+const app = new cdk.App();
+new GoogleCalendarApi(app, "GoogleCalendarApi", {});
--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -1,17 +1,43 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+/** @format */
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+import { App } from "aws-cdk-lib";
+import { GoogleCalendarApi } from "../lib/google_calendar_api";
+import { Template } from "aws-cdk-lib/assertions";
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+describe("GoogleCalendarApi Stack", () => {
+  const app = new App();
+  const stack = new GoogleCalendarApi(app, "TestStack");
+
+  it("creates an IAM role", () => {
+    const assert = Template.fromStack(stack);
+    assert.resourceCountIs("AWS::IAM::Role", 1);
+    assert.hasResourceProperties("AWS::IAM::Role", {
+      AssumeRolePolicyDocument: {
+        Statement: [
+          {
+            Action: "sts:AssumeRole",
+            Effect: "Allow",
+            Principal: {
+              Service: "lambda.amazonaws.com",
+            },
+          },
+        ],
+        Version: "2012-10-17",
+      },
+    });
+  });
+
+  it("creates a Lambda layer", () => {
+    const assert = Template.fromStack(stack);
+    assert.resourceCountIs("AWS::Lambda::LayerVersion", 1);
+  });
+
+  it("creates a Lambda function", () => {
+    const assert = Template.fromStack(stack);
+    assert.resourceCountIs("AWS::Lambda::Function", 1);
+    assert.hasResourceProperties("AWS::Lambda::Function", {
+      Handler: "main.handler",
+      Runtime: "python3.11",
+    });
+  });
 });
